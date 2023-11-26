3fd02bcf0d9eae172434991cfb4aca3e15b21242
mkalman01 <mkalman01@gmail.com>
2023-11-26 14:09:13 +0200

    updated models.py

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -64,3 +64,11 @@ class ProfileFeedItem(models.Model):
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE
     )
+
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        """Return the model as a string"""
+        return self.status_text
